3b27dd5fe1bd988703e9cd8c84ca5c5c984d76b8
Hikl845 <freelancer198756@gmail.com>
2025-10-07 00:07:35 +0300

    Update Quad.java

--- src/main/java/org/example/Quad.java
+++ src/main/java/org/example/Quad.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Quad extends Shape {
-    private String name = "Quad";;
+    private String name = "Quad";
+    
     String Quad(){
         return name;
     }
