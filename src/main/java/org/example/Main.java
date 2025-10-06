eca6b0da7c83e6a94ab43bb5a7374f7e5589cdbc
Hikl845 <freelancer198756@gmail.com>
2025-10-07 00:10:46 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    
     public static int getValidInt(Scanner scanner) {
         while (!scanner.hasNextInt()) {
             System.out.print("Ошибка. Введите целое число: ");
@@ -12,27 +13,28 @@ public class Main {
         }
         return scanner.nextInt();
     }
+    
     public static void main(String[] args) {
-        int numb1;
-        int numb2;
-        int numb3;
-        int numb4;
-        int numb5;
         ShapeChecker checker = new ShapeChecker();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Сторона 1/ Радіус: ");
+         int numb1;
         numb1 = getValidInt(scanner);
 
         System.out.print("Сторона 2: ");
+        int numb2;
         numb2 = getValidInt(scanner);
 
         System.out.print("Сторона 3: ");
+        int numb3;
         numb3 = getValidInt(scanner);
 
         System.out.print("Сторона 4: ");
+        int numb4;
         numb4 = getValidInt(scanner);
 
         System.out.print("Сторона 5: ");
+        int numb5;
         numb5 = getValidInt(scanner);
         checker.setSides( numb1, numb2, numb3, numb4, numb5);
         String result = checker.Detect();
